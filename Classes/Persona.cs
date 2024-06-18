20d9f97a633b9e99663e4d631b927f3de8bc950c
Riscaldino <Riscaldino@RISCALDINO-PRO>
2024-06-18 16:16:32 +0200

    Aggiungere i file di progetto.

---
+++ Classes/Persona.cs
@@ -0,0 +1,45 @@
+﻿/*
+ Pratica S1/L2
+Scrivere una classe Persona con le proprietà Nome, Cognome, Eta e con metodi GetNome, GetCognome, GetEta e GetDettagli che restituisca in una stringa le informazioni sulla persona in questione.
+P.S.: GetDettagli restituisce tutti i dettagli (Nome, cognome ed età della persona).
+Utilizzare una Console Application per stampare i dati dei metodi. Lasciare al candidato la scelta se utilizzare costruttori per la popolazione degli oggetti o istanziare le proprietà nella classe Program.
+ */
+
+using System;
+
+public class Persona
+{
+    // Proprietà
+    public string Nome { get; set; }
+    public string Cognome { get; set; }
+    public int Eta { get; set; }
+
+    // Costruttore
+    public Persona(string nome, string cognome, int eta)
+    {
+        Nome = nome;
+        Cognome = cognome;
+        Eta = eta;
+    }
+
+    // Metodi
+    public string GetNome()
+    {
+        return Nome;
+    }
+
+    public string GetCognome()
+    {
+        return Cognome;
+    }
+
+    public int GetEta()
+    {
+        return Eta;
+    }
+
+    public string GetDettagli()
+    {
+        return $"Nome: {Nome}, Cognome: {Cognome}, Età: {Eta}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creazione di un'istanza della classe Persona
+        Persona persona = new Persona("Mario", "Rossi", 30);
+
+        // Stampa dei dati 
+        Console.WriteLine("Nome: " + persona.GetNome());
+        Console.WriteLine("Cognome: " + persona.GetCognome());
+        Console.WriteLine("Età: " + persona.GetEta());
+        Console.WriteLine("Dettagli: " + persona.GetDettagli());
+
+    }
+}
