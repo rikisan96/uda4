73377a89715f423cbed840054d15a043eb53fcdc
rikisan96 <riccardocutruneo@gmail.com>
2024-07-17 21:59:56 +0200

    added data e AccService

---
+++ ProgettoIntrasettimanale/ApplicationLayer/Data/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+public class User
+{
+    public int Id { get; set; }
+    public required string  Username { get; set; }
+    public required string Password { get; set; }
+    public string? Email { get; set; }
+    public List<string>? UserType { get; set; } = [];       //Gli utenti base saranno aziende e privati, gli admin saranno gli amministratori del sito
+
+    public User()
+    {
+        // Inizializzazione delle proprietà, se necessario
+    }
+}
---
+++ ProgettoIntrasettimanale/ApplicationLayer/IAccountService.cs
@@ -0,0 +1,22 @@
+﻿namespace ApplicationLayer;
+
+
+public interface IAccountService
+{
+    User Register(User Username);
+
+    User? Login(string Username, string Password);
+
+    User? GetByUsername(string Username);
+
+    bool AddUserType(string Username, string UserType);
+
+    bool RemoveUserType(string Username, string UserType); 
+
+    List<User> GetAllUsers();
+
+    List<string> GetAllUserType();
+
+
+    
+}
