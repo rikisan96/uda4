fe63418274d0d2503d9b8d3d9a3a1113b3aa8cbf
rikisan96 <riccardocutruneo@gmail.com>
2024-07-19 16:53:02 +0200

    Created new project w5-provaVenerdi

---
+++ w5-progettoVenerdi/w5-progettoVenerdi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace w5_progettoVenerdi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
